47a23dfc86d14f045144e83b9ad47576ed9c1c2d
eltigerchino <chewteeming01@gmail.com>
2024-10-08 14:22:01 +0800

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div>
+	<nav>		
+		<a href="#dialog-main">Open dialog</a>
+        <a href="/">Close dialog</a>
+	</nav>
+	<main>
+		{#if $page.url.hash === '#dialog-main'}
+			<div class="dialog" id="dialog-main">
+				<h2>Hello from dialog</h2>
+				<a href={$page.url.pathname}>Close dialog</a>
+			</div>
+		{/if}
+	</main>
+	<footer>
+			<pre>Hash is: {$page.url.hash}</pre>
+	</footer>
+</div>
+
+<style>
+.dialog {
+	border: 2px solid rebeccapurple;
+}
+</style>
